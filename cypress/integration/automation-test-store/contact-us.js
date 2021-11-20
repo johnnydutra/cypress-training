cb8f5a6002e3f5e86d800909500d42df9a94359a
Johnny Dutra <johnny.dutra@gmail.com>
2021-11-20 10:10:37 -0300

    Creates first test for Automation Test Store

---
+++ cypress/integration/automation-test-store/contact-us.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+
+describe("Test Contact Us Form via Automation Test Store", () => {
+    it("Should be able to submit a successful submission via Contact Us form", () => {
+        cy.visit("http://www.automationteststore.com/");
+
+    });
+});
